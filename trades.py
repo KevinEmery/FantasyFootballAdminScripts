9ca03c3215088fcec9c32a7c27ec653f13075c2f
Kevin Emery <360emery@gmail.com>
2022-08-17 22:57:15 -0700

    Add traded faab to displayed trade details
    Previously the logic only handled draft picks and players,
    but now if FAAB is traded as part of a trade it will be
    included in the output

--- trades.py
+++ trades.py
@@ -42,6 +42,8 @@ class TeamTradeDetails:
         self.losses = []
         self.draft_picks_added = []
         self.draft_picks_lost = []
+        self.faab_added = 0
+        self.faab_lost = 0
 
     def add_player(self, player_id: str):
         self.adds.append(player_id)
@@ -69,6 +71,12 @@ class TeamTradeDetails:
         self.draft_picks_lost.append(season + " " +
                                      self._append_round_suffix(round))
 
+    def add_faab(self, faab: int):
+        self.faab_added = faab
+
+    def lose_faab(self, faab: int):
+        self.faab_lost = faab
+
     # Only used for debugging
     def __str__(self):
         return_string = ""
@@ -78,11 +86,15 @@ class TeamTradeDetails:
             return_string += add + "\n"
         for add in self.draft_picks_added:
             return_string += add + "\n"
+        if self.faab_added > 0:
+            return_string += "$" + str(self.faab_added) + "\n"
         return_string += "--Lost--\n"
         for loss in self.losses:
             return_string += loss + "\n"
         for loss in self.draft_picks_lost:
             return_string += loss + "\n"
+        if self.faab_lost > 0:
+            return_string += "$" + str(self.faab_lost) + "\n"
         return_string += "\n"
         return return_string
 
@@ -90,7 +102,8 @@ class TeamTradeDetails:
 class Trade:
     def __init__(self, timestamp_in_millis: int, involved_rosters: List[int],
                  adds: Dict[str, int], drops: Dict[str, int],
-                 draft_picks: List[Dict[str, str]]):
+                 draft_picks: List[Dict[str, str]], faab: List[Dict[str,
+                                                                    int]]):
         self.trade_time = datetime.fromtimestamp(timestamp_in_millis / 1000)
         self.roster_id_to_detail = {}
 
@@ -102,6 +115,7 @@ class Trade:
         self._process_adds(adds)
         self._process_drops(drops)
         self._process_draft_picks(draft_picks)
+        self._process_faab(faab)
 
     # Record every add in the trade onto the appropriate roster
     def _process_adds(self, adds: Dict[str, int]):
@@ -127,6 +141,13 @@ class Trade:
                 pick["previous_owner_id"]].lose_draft_pick(
                     pick["season"], pick["round"])
 
+    def _process_faab(self, faab: List[Dict[str, int]]):
+        for line_item in faab:
+            self.roster_id_to_detail[line_item["sender"]].lose_faab(
+                line_item["amount"])
+            self.roster_id_to_detail[line_item["receiver"]].add_faab(
+                line_item["amount"])
+
     def __lt__(self, other):
         return self.trade_time < other.trade_time
 
@@ -151,7 +172,8 @@ def fetch_all_league_trades(league: League) -> List[Trade]:
                     Trade(transaction.get("status_updated"),
                           transaction.get("consenter_ids"),
                           transaction.get("adds"), transaction.get("drops"),
-                          transaction.get("draft_picks")))
+                          transaction.get("draft_picks"),
+                          transaction.get("waiver_budget")))
 
     return all_trades
 
@@ -202,11 +224,15 @@ def print_two_team_trade(league_id: str, trade: Trade,
         team_a_adds.append(format_player_string(player_id, player_map))
     for pick in trade_detail.draft_picks_added:
         team_a_adds.append(pick)
+    if trade_detail.faab_added > 0:
+        team_a_adds.append(format_faab(trade_detail.faab_added))
 
     for player_id in trade_detail.losses:
         team_b_adds.append(format_player_string(player_id, player_map))
     for pick in trade_detail.draft_picks_lost:
         team_b_adds.append(pick)
+    if trade_detail.faab_lost > 0:
+        team_b_adds.append(format_faab(trade_detail.faab_lost))
 
     # Output the trade itself
     print("Trade on " + trade.trade_time.strftime(date_template))
@@ -221,7 +247,7 @@ def print_two_team_trade(league_id: str, trade: Trade,
                                 roster_link=create_roster_link(
                                     league_id, trade_rosters[1])))
 
-    # Preferred format, but looks bad on Desktop
+    # Preferred format, but looks bad on mobile
     # print_side_by_side_table(team_a_adds, team_b_adds)
 
     # Formats well on desktop and mobile
@@ -294,11 +320,15 @@ def print_larger_trade(league_id: str, trade: Trade, roster_id_map: Dict[int,
             print("    " + format_player_string(player_id, player_map))
         for pick in trade_detail.draft_picks_added:
             print("    " + pick)
+        if trade_detail.faab_added > 0:
+            print("    " + format_faab(trade_detail.faab_added))
         print("*Traded Away*")
         for player_id in trade_detail.losses:
             print("    " + format_player_string(player_id, player_map))
         for pick in trade_detail.draft_picks_lost:
             print("    " + pick)
+        if trade_detail.faab_lost > 0:
+            print("    " + format_faab(trade_detail.faab_lost))
         print("")
 
 
@@ -313,6 +343,11 @@ def format_player_string(player_id: str, player_map) -> str:
     return template.format(first=player["first_name"],
                            last=player["last_name"],
                            position=player["position"])
+
+
+def format_faab(faab: int) -> str:
+    template = "${number} FAAB"
+    return template.format(number=faab)
 
 
 def parse_user_provided_flags() -> argparse.Namespace:
