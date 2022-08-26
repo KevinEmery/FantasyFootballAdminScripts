d162460d7683d46fae96ff448ded2a44512d4fff
Kevin Emery <360emery@gmail.com>
2022-08-26 15:24:28 -0700

    Move platform selection enum to common folder

--- common.py
+++ common.py
@@ -1,9 +1,14 @@
 import re
 
+from enum import Enum
 from typing import List
 
 from library.model.league import League
 
+
+class PlatformSelection(Enum):
+    SLEEPER = 1
+    FLEAFLICKER = 2
 
 def filter_leagues_by_league_name(leagues: List[League],
                                   name_regex: re.Pattern) -> List[League]:
