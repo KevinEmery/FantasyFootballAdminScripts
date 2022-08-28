d53b55d71617b9dcb61ab3459e0ee510f0c404cb
Kevin Emery <360emery@gmail.com>
2022-08-28 12:38:55 -0700

    Update transaction to have a comparison operator for sorting

--- library/model/transaction.py
+++ library/model/transaction.py
@@ -4,7 +4,10 @@ from .team import Team
 
 
 class Transaction(object):
-    def __init__(self, timestamp: int, transaction_type: str, team: Team):
-        self.timestamp = timestamp
+    def __init__(self, time: int, transaction_type: str, team: Team):
+        self.time = time
         self.transaction_type = transaction_type
         self.team = team
+
+    def __lt__(self, other):
+        return self.time < other.time
