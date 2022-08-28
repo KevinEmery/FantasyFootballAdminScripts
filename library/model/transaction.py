7259970ccf9534699e7e5a2d64a5e0927f835c25
Kevin Emery <360emery@gmail.com>
2022-08-28 12:01:59 -0700

    Modify Transaction object to only contain a single team
    For our purposes we don't need transactions to contain a list of teams
    but instead only a single team, because when we're using them we only
    want to tie them to a specific team.

--- library/model/transaction.py
+++ library/model/transaction.py
@@ -4,8 +4,7 @@ from .team import Team
 
 
 class Transaction(object):
-    def __init__(self, timestamp: int, transaction_type: str,
-                 teams: List[Team]):
+    def __init__(self, timestamp: int, transaction_type: str, team: Team):
         self.timestamp = timestamp
         self.transaction_type = transaction_type
-        self.teams = teams
+        self.team = team
