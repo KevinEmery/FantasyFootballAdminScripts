c84e87a63908e63181ddd1f5fa0361bc10ba2325
Kevin Emery <360emery@gmail.com>
2023-07-16 23:08:48 -0700

    Update ADP message content to use embeds instead of code block
    After using this for a few days, several people have called out that the
    code block formatting doesn't look good on mobile (and that it's
    non-obvious why it looks bad). Ultimately want I want is a table format,
    but Discord markdown doesn't support that.
    
    So instead, I'm moving to using Discord embeds that put the player
    name on one line (as the "title") and then all of the data in a
    single-line code segment (for spacing) below it (as the "value"). This
    creates a more pleasant viewing experience on both mobile and desktop,
    without detracting from the raw data and the ability to parse it.
    
    Since we're using embeds, I'm leveraging the embed color and matching it
    to Sleeper's colors for each position, at least for the FTA posts. I
    imagine more refactoring will come around once I add this for NarFFL as
    well, but that's to come later.

--- discord_bot.py
+++ discord_bot.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from discord.ext import commands
 from typing import List
 
-MESSAGE_LENGTH_LIMIT = 2000
-MARKDOWN_CODE_BLOCK_BOUNDARY = "```\n"
+# Actual limit is 25, we want to steer clear in case we add fields on top of the iteration
+EMBED_FIELD_LIMIT = 20
 FTA_ADP_THREAD_CONTENT = "The data here is for the FTA league format. \
 These leagues are 14-team, 0.5 PPR Leagues that start \
 1 QB, 2 RBs, 3 WRs, 1 TE, 1 W/R/T Flex, 1 K, and 1 DEF.\n\n"
@@ -18,8 +18,6 @@ Max: The latest a player was drafted\n\
 N:   The number of times a player has been drafted\
 ```\n\
 The designation \"X.Y\" represents a selection in Round X, at Pick Y"
-ADP_FORMAT_NOTE = "Data is best viewed with a Desktop client. Each \
-player should be on their own line."
 
 intents = discord.Intents.default()
 intents.members = True
@@ -47,72 +45,77 @@ async def post_fta_adps(ctx, forum: discord.ForumChannel):
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_all(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, adp.INCLUDE_ALL, "All Players")
+    await _post_fta_position_adp(ctx, forum, adp.INCLUDE_ALL, "All Players", discord.Colour.dark_blue())
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_qb(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "QB", "Quarterback")
+    await _post_fta_position_adp(ctx, forum, "QB", "Quarterback", discord.Colour.from_rgb(192, 94, 133))
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_wr(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "WR", "Wide Receiver")
+    await _post_fta_position_adp(ctx, forum, "WR", "Wide Receiver", discord.Colour.from_rgb(70, 162, 202))
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_rb(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "RB", "Running Back")
+    await _post_fta_position_adp(ctx, forum, "RB", "Running Back", discord.Colour.from_rgb(115, 195, 166))
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_te(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "TE", "Tight End")
+    await _post_fta_position_adp(ctx, forum, "TE", "Tight End", discord.Colour.from_rgb(204, 140, 74))
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_k(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "K", "Kicker")
+    await _post_fta_position_adp(ctx, forum, "K", "Kicker", discord.Colour.purple())
 
 
 @bot.command()
 @commands.has_any_role("Commissioner", "League Admin")
 async def post_fta_adp_def(ctx, forum: discord.ForumChannel):
-    await _post_fta_position_adp(ctx, forum, "DEF", "Team Defense")
+    await _post_fta_position_adp(ctx, forum, "DEF", "Team Defense", discord.Colour.from_rgb(154, 95, 78))
 
 
-async def _post_fta_position_adp(ctx, forum: discord.ForumChannel, position_short: str, position_long: str):
-    adp_data = adp.aggregate_adp_data("FTAFFL", 14, position=position_short, league_regex_string="^FTA \#\d+.*$")
-    messages = _break_adp_content_into_messages(adp_data)
+async def _post_fta_position_adp(ctx, forum: discord.ForumChannel, position_short: str, position_long: str, embed_color: discord.Colour):
+    adp_data = adp.aggregate_adp_data("FTAFFL", 14, position=position_short,
+                                      league_regex_string="^FTA \#\d+.*$", output_format=adp.OutputFormat.FORMATTED_CSV)
+    messages = _break_adp_content_into_messages(adp_data, embed_color)
     thread_title = _get_formatted_date() + ": " + position_long
-    thread_content = FTA_ADP_THREAD_CONTENT + ADP_GLOSSARY + ADP_FORMAT_NOTE
+    thread_content = FTA_ADP_THREAD_CONTENT + ADP_GLOSSARY
     thread = (await forum.create_thread(name=thread_title, content=thread_content))[0]
     for message in messages:
-        await thread.send(message)
+        await thread.send(embed=message)
 
 
-def _break_adp_content_into_messages(content: List[str], title: str = "") -> List[str]:
+def _break_adp_content_into_messages(content: List[str], embed_color: discord.Colour) -> List[discord.Embed]:
     split_content = []
-    current_content = title + "\n\n" + MARKDOWN_CODE_BLOCK_BOUNDARY if title else MARKDOWN_CODE_BLOCK_BOUNDARY
+    current_embed = discord.Embed(colour=embed_color)
 
     for line in content:
-        if (MESSAGE_LENGTH_LIMIT - len(current_content)) < 2 * len(line):
-            current_content += MARKDOWN_CODE_BLOCK_BOUNDARY
-            split_content.append(current_content)
-            current_content = MARKDOWN_CODE_BLOCK_BOUNDARY
+        if (len(current_embed.fields) >= EMBED_FIELD_LIMIT):
+            split_content.append(current_embed)
+            current_embed = discord.Embed(colour=embed_color)
+        _convert_adp_csv_to_embed_field(line, current_embed)
 
-        current_content += line + "\n"
-
-    if current_content != MARKDOWN_CODE_BLOCK_BOUNDARY:
-        current_content += MARKDOWN_CODE_BLOCK_BOUNDARY
-        split_content.append(current_content)
+    if len(current_embed.fields) > 0:
+        split_content.append(current_embed)
 
     return split_content
+
+
+def _convert_adp_csv_to_embed_field(content: str, embed: discord.Embed):
+    player_data = content.split(",")
+    template = "`ADP: {adp:<5} Min: {min:<5} Max: {max:<5} N: {n}`"
+    embed.add_field(name=player_data[0], value=template.format(n=player_data[4],
+                    adp=player_data[1], min=player_data[2], max=player_data[3]), inline=False)
 
 
 def _get_formatted_date() -> str:
