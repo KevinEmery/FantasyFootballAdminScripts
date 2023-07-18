bc410e0ec318179108cf942c57a9882ae67a5f85
Kevin Emery <360emery@gmail.com>
2023-07-18 00:01:28 -0700

    Add bot commands to periodically post FTA trades
    This leverages the recent changes to the trade script, creating a
    command that polls for trades every 30 minutes. When a trade is posted,
    the most recent timestamp is stored locally so that we only post new
    trades (hopefully) and don't re-post existing ones.
    
    All of this is running with a looping task, that is started and stopped
    using specific commands.
    
    Because this requires the bot to maintain state, there are two local
    files under a new `bot_data` directory that store the channel to write
    to as well as the last trade timestamp.

--- discord_bot.py
+++ discord_bot.py
@@ -1,8 +1,11 @@
-import adp
 import discord
+import os
+
+import adp
+import trades
 
 from datetime import datetime
-from discord.ext import commands
+from discord.ext import commands, tasks
 from typing import List
 
 # Actual limit is 25, we want to steer clear in case we add fields on top of the iteration
@@ -19,6 +22,12 @@ N:   The number of times a player has been drafted\
 ```\n\
 The designation \"X.Y\" represents a selection in Round X, at Pick Y"
 
+FTA_TRADE_CHANNEL_PATH = "./bot_data/fta_trade_channel"
+FTA_LAST_TRADE_TIMESTAMP_PATH = "./bot_data/fta_last_trade_timestamp"
+
+FTAFFL_USER = "FTAFFL"
+FTAFFL_LEAGUE_REGEX = "^FTA \#\d+.*$"
+
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -30,6 +39,8 @@ bot = commands.Bot(command_prefix='&', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
+
+# FTA ADP Commands
 
 
 @bot.command()
@@ -85,8 +96,8 @@ async def post_fta_adp_def(ctx, forum: discord.ForumChannel):
 
 
 async def _post_fta_position_adp(ctx, forum: discord.ForumChannel, position_short: str, position_long: str, embed_color: discord.Colour):
-    adp_data = adp.aggregate_adp_data("FTAFFL", 14, position=position_short,
-                                      league_regex_string="^FTA \#\d+.*$", output_format=adp.OutputFormat.FORMATTED_CSV)
+    adp_data = adp.aggregate_adp_data(FTAFFL_USER, 14, position=position_short,
+                                      league_regex_string=FTAFFL_LEAGUE_REGEX, output_format=adp.OutputFormat.FORMATTED_CSV)
     messages = _break_adp_content_into_messages(adp_data, embed_color)
     thread_title = _get_formatted_date() + ": " + position_long
     thread_content = FTA_ADP_THREAD_CONTENT + ADP_GLOSSARY
@@ -121,6 +132,83 @@ def _convert_adp_csv_to_embed_field(content: str, embed: discord.Embed):
 def _get_formatted_date() -> str:
     now = datetime.now()
     return now.strftime("%m/%d/%y")
+
+# FTA Trade Commands
+
+
+@bot.command()
+@commands.has_any_role("Commissioner", "League Admin")
+async def start_posting_fta_trades(ctx):
+    post_fta_trades.start()
+
+
+@bot.command()
+@commands.has_any_role("Commissioner", "League Admin")
+async def stop_posting_fta_trades(ctx):
+    post_fta_trades.cancel()
+
+
+@tasks.loop(minutes=30.0)
+async def post_fta_trades():
+    trade_channel = _get_fta_trade_channel()
+
+    if trade_channel is not None:
+        # Get the last timestamp, determine if we need to pass it to the method as a starting point
+        last_posted_trade_timestamp = _read_fta_last_trade_timestamp()
+
+        if last_posted_trade_timestamp != "":
+            print("FTA_Trades: Retrieving trades since: " + last_posted_trade_timestamp)
+            all_trades = trades.fetch_and_filter_trades(
+                FTAFFL_USER, league_regex_string=FTAFFL_LEAGUE_REGEX, start_date_string=last_posted_trade_timestamp)
+        else:
+            print("FTA_Trades: No previous timestamp found")
+            all_trades = trades.fetch_and_filter_trades(FTAFFL_USER, league_regex_string=FTAFFL_LEAGUE_REGEX)
+
+        # Post the trades
+        for trade in all_trades:
+            await trade_channel.send(content=trades.format_trades([trade]))
+
+        # Write the timestamp of the last trade
+        if len(all_trades) > 0:
+            _write_fta_last_trade_timestamp(all_trades[-1].trade_time)
+    else:
+        print("FTA_Trades: No trade channel available")
+
+
+@bot.command()
+@commands.has_any_role("Commissioner", "League Admin")
+async def set_fta_trades_channel(ctx, channel: discord.TextChannel):
+    file = open(FTA_TRADE_CHANNEL_PATH, "w")
+    file.write(str(channel.id))
+    file.close()
+
+
+def _get_fta_trade_channel() -> discord.TextChannel:
+    if os.path.isfile(FTA_TRADE_CHANNEL_PATH):
+        file = open(FTA_TRADE_CHANNEL_PATH, "r")
+        channel_id = file.read()
+        file.close()
+    else:
+        return None
+
+    return bot.get_channel(int(channel_id))
+
+
+def _write_fta_last_trade_timestamp(timestamp: str):
+    file = open(FTA_LAST_TRADE_TIMESTAMP_PATH, "w")
+    file.write(str(timestamp))
+    file.close()
+
+
+def _read_fta_last_trade_timestamp() -> str:
+    if os.path.isfile(FTA_LAST_TRADE_TIMESTAMP_PATH):
+        file = open(FTA_LAST_TRADE_TIMESTAMP_PATH, "r")
+        timestamp = file.read()
+        file.close()
+    else:
+        return ""
+
+    return timestamp
 
 
 def _retrieve_token() -> str:
