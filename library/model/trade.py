e0863e03bef380865cfb7068dee29a8545892813
Kevin Emery <360emery@gmail.com>
2023-07-20 16:09:13 -0700

    Add custom hashing function to Trade class
    This is in preparation for better dup detection on trades, so that I can
    know if we've posted a specific trade before or not.
    
    I originally implemented this with the built in Python hash functions
    and classes. But apparently an object's hash isn't consistent between
    runtimes, so I can't write a hash and then have it be the same hash in a
    later run. Which defeats the purpose of persisting the hash. So instead
    I'm using hashlib to do an md5 hash of the trade details.
    
    If a league has more than one trade in a given second they'll come up as
    duplicates, which I'm fine with for now.

--- library/model/trade.py
+++ library/model/trade.py
@@ -1,5 +1,5 @@
 """
-   Copyright 2022 Kevin Emery
+   Copyright 2023 Kevin Emery
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+import hashlib
 
 from datetime import datetime
 from typing import List
@@ -29,3 +30,14 @@ class Trade(object):
 
     def __lt__(self, other):
         return self.trade_time < other.trade_time
+
+    def get_string_trade_time(self):
+        return self.trade_time.strftime("%m/%d/%Y - %H:%M:%S")
+
+    def hash(self):
+        hashId = hashlib.md5()
+        temp = self.league.name + self.get_string_trade_time()
+
+        hashId.update(temp.encode('utf-8'))
+
+        return hashId.hexdigest()
