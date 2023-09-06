9fc31a26bb5449723d6b38fd9cbb3f6a8ca9bb01
Kevin Emery <360emery@gmail.com>
2023-09-06 13:44:57 -0700

    Refactor LeagueScore script to use a helper method
    In preparation for using the logic here programatically, the bulk of the
    script logic has been moved into a helper method. As a part of that, and
    in order to reduce the number of API calls made, that helper method
    returns the results in bulk in a single custom object.
    
    An alternative approach here that was considerted but discarded was
    having dedicated methods for a single set of scoring results (in lieu of
    the bulk object) but that was deemed to expensive and unnecessary when a
    custom object could be used.

--- leaguescoring.py
+++ leaguescoring.py
@@ -19,15 +19,112 @@ import re
 import sys
 
 import common
+import library.common as libCommon
 
-from library.model.league import League
+from typing import List
+
 from library.model.seasonscore import SeasonScore
-from library.model.user import User
 from library.model.weeklyscore import WeeklyScore
 
-from library.platforms.platform import Platform
 from library.platforms.fleaflicker.fleaflicker import Fleaflicker
 from library.platforms.sleeper.sleeper import Sleeper
+
+
+DEFAULT_YEAR = libCommon.DEFAULT_YEAR
+DEFAULT_LEAGUE_REGEX_STRING = ".*"
+DEFAULT_PLATFORM = common.PlatformSelection.SLEEPER
+
+
+class ScoringResults(object):
+    def __init__(self):
+        self.max_weekly_scores: List[WeeklyScore] = []
+        self.min_weekly_scores: List[WeeklyScore] = []
+        self.max_scores_this_week: List[WeeklyScore] = []
+        self.min_scores_this_week: List[WeeklyScore] = []
+        self.max_season_scores: List[SeasonScore] = []
+        self.min_season_scores: List[SeasonScore] = []
+
+
+def get_scoring_results(
+    account_identifier: str,
+    starting_week: int,
+    ending_week: int,
+    get_weekly_results: bool,
+    get_current_weeks_results: bool,
+    get_season_results: bool,
+    get_max_scores: bool,
+    get_min_scores: bool,
+    year: int = DEFAULT_YEAR,
+    league_regex_string: str = DEFAULT_LEAGUE_REGEX_STRING,
+    platform: common.PlatformSelection = DEFAULT_PLATFORM,
+) -> ScoringResults:
+
+    # Lists containing the raw data from the backend
+    weekly_scores = []
+    season_scores = []
+    results = ScoringResults()
+
+    find_weekly = get_weekly_results or get_current_weeks_results
+    find_max_this_week = get_max_scores and get_current_weeks_results
+    find_min_this_week = get_min_scores and get_current_weeks_results
+    find_max_weekly = get_weekly_results and get_max_scores
+    find_min_weekly = get_weekly_results and get_min_scores
+    find_season = get_season_results
+    find_max_season = get_season_results and get_max_scores
+    find_min_season = get_season_results and get_min_scores
+
+    user = platform.get_admin_user_by_identifier(account_identifier)
+    leagues = platform.get_all_leagues_for_user(user, year, re.compile(league_regex_string))
+
+    for league in leagues:
+        # Iterate over each indiviudal week, since we need to grab the max weekly score for the league
+        if find_weekly:
+            for week_num in range(starting_week, ending_week + 1):
+                weekly_scores.extend(
+                    platform.get_weekly_scores_for_league_and_week(
+                        league, week_num, year))
+
+        if find_season:
+            # Grab the points-for in each league
+            season_scores.extend(
+                platform.get_season_scores_for_league(league, year))
+
+    # Sort all of the lists
+    if find_season:
+        max_season_scores = season_scores.copy()
+        max_season_scores.sort(
+            key=lambda annual_score: annual_score.score, reverse=True)
+        min_season_scores = season_scores.copy()
+        min_season_scores.sort(
+            key=lambda annual_score: annual_score.score)
+    if find_weekly:
+        max_weekly_scores = weekly_scores.copy()
+        max_weekly_scores.sort(key=lambda weekly_score: weekly_score.score,
+                               reverse=True)
+        min_weekly_scores = weekly_scores.copy()
+        min_weekly_scores.sort(key=lambda weekly_score: weekly_score.score)
+        max_scores_this_week = list(
+            filter(lambda weekly_score: weekly_score.week == ending_week,
+                   max_weekly_scores.copy()))
+        min_scores_this_week = list(
+            filter(lambda weekly_score: weekly_score.week == ending_week,
+                   min_weekly_scores.copy()))
+
+    # Construct the results object based on the specified flags
+    if find_max_this_week:
+        results.max_scores_this_week = max_scores_this_week
+    if find_min_this_week:
+        results.min_scores_this_week = min_scores_this_week
+    if find_max_weekly:
+        results.max_weekly_scores = max_weekly_scores
+    if find_min_weekly:
+        results.min_weekly_scores = min_weekly_scores
+    if find_max_season:
+        results.max_season_scores = max_season_scores
+    if find_min_season:
+        results.min_season_scores = min_season_scores
+
+    return results
 
 
 def parse_user_provided_flags() -> argparse.Namespace:
@@ -50,13 +147,13 @@ def parse_user_provided_flags() -> argparse.Namespace:
         "--year",
         help="The year to run the analysis on, defaults to 2023",
         type=int,
-        default=2023)
+        default=DEFAULT_YEAR)
     parser.add_argument(
         "-r",
         "--league_regex",
         help="Regular expression used to select which leagues to analyze",
         type=str,
-        default=".*")
+        default=DEFAULT_LEAGUE_REGEX_STRING)
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--max",
                        dest="max",
@@ -124,7 +221,7 @@ def parse_user_provided_flags() -> argparse.Namespace:
                         season=True,
                         weekly=True,
                         current_week=True,
-                        platform_selection=common.PlatformSelection.SLEEPER)
+                        platform_selection=DEFAULT_PLATFORM)
 
     return parser.parse_args()
 
@@ -136,19 +233,16 @@ def main(argv):
     # Convert the computed args into our more-verbose local fields
     identifier = args.identifier
     year = args.year
-    league_regex = re.compile(args.league_regex)
+    league_regex_string = args.league_regex
     starting_week = args.start
     ending_week = args.end
+    get_weekly = args.weekly
+    get_season = args.season
+    get_min = args.min
+    get_max = args.max
+    get_current_week = args.current_week
     weekly_score_output_count = args.weekly_count
     seasonal_score_output_count = args.season_count
-    find_weekly = args.weekly or args.current_week
-    print_max_this_week = args.max and args.current_week
-    print_min_this_week = args.min and args.current_week
-    print_max_weekly = args.weekly and args.max
-    print_min_weekly = args.weekly and args.min
-    find_seasonal = args.season
-    print_max_seasonal = args.season and args.max
-    print_min_seasonal = args.season and args.min
 
     # Set platform based on user choice
     if args.platform_selection == common.PlatformSelection.SLEEPER:
@@ -156,84 +250,38 @@ def main(argv):
     elif args.platform_selection == common.PlatformSelection.FLEAFLICKER:
         platform = Fleaflicker()
 
-    # Lists containing all of the collated data
-    weekly_scores = []
-    seasonal_scores = []
-
-    user = platform.get_admin_user_by_identifier(identifier)
-    leagues = platform.get_all_leagues_for_user(user, year, league_regex)
-
-    for league in leagues:
-        # Iterate over each indiviudal week, since we need to grab the max weekly score for the league
-        if find_weekly:
-            for week_num in range(starting_week, ending_week + 1):
-                weekly_scores.extend(
-                    platform.get_weekly_scores_for_league_and_week(
-                        league, week_num, year))
-
-        if find_seasonal:
-            # Grab the points-for in each league
-            seasonal_scores.extend(
-                platform.get_season_scores_for_league(league, year))
-
-    # Sublists used for sorted results
-    max_weekly_scores = []
-    min_weekly_scores = []
-    max_scores_this_week = []
-    min_scores_this_week = []
-    max_seasonal_points_for = []
-    min_seasonal_points_for = []
-
-    # Sort all of the lists
-    if find_seasonal:
-        max_seasonal_points_for = seasonal_scores.copy()
-        max_seasonal_points_for.sort(
-            key=lambda annual_score: annual_score.score, reverse=True)
-        min_seasonal_points_for = seasonal_scores.copy()
-        min_seasonal_points_for.sort(
-            key=lambda annual_score: annual_score.score)
-    if find_weekly:
-        max_weekly_scores = weekly_scores.copy()
-        max_weekly_scores.sort(key=lambda weekly_score: weekly_score.score,
-                               reverse=True)
-        min_weekly_scores = weekly_scores.copy()
-        min_weekly_scores.sort(key=lambda weekly_score: weekly_score.score)
-        max_scores_this_week = list(
-            filter(lambda weekly_score: weekly_score.week == ending_week,
-                   max_weekly_scores.copy()))
-        min_scores_this_week = list(
-            filter(lambda weekly_score: weekly_score.week == ending_week,
-                   min_weekly_scores.copy()))
+    results = get_scoring_results(identifier, starting_week, ending_week, get_weekly,
+                                  get_current_week, get_season, get_max, get_min, year, league_regex_string, platform)
 
     # Print out the results
     this_week_template = "{main_header}, Week {week_num}"
-    if print_max_this_week:
+    if results.max_scores_this_week:
         common.print_weekly_scores_with_header(
-            max_scores_this_week,
+            results.max_scores_this_week,
             this_week_template.format(main_header="HIGHEST SCORES THIS WEEK",
                                       week_num=ending_week),
             weekly_score_output_count)
-    if print_min_this_week:
+    if results.min_scores_this_week:
         common.print_weekly_scores_with_header(
-            min_scores_this_week,
+            results.min_scores_this_week,
             this_week_template.format(main_header="LOWEST SCORES THIS WEEK",
                                       week_num=ending_week),
             weekly_score_output_count)
-    if print_max_weekly:
+    if results.max_weekly_scores:
         common.print_weekly_scores_with_header(
-            max_weekly_scores, "HIGHEST WEEKLY SCORES THIS SEASON",
+            results.max_weekly_scores, "HIGHEST WEEKLY SCORES THIS SEASON",
             weekly_score_output_count)
-    if print_min_weekly:
+    if results.min_weekly_scores:
         common.print_weekly_scores_with_header(
-            min_weekly_scores, "LOWEST WEEKLY SCORES THIS SEASON",
+            results.min_weekly_scores, "LOWEST WEEKLY SCORES THIS SEASON",
             weekly_score_output_count)
-    if print_max_seasonal:
+    if results.max_season_scores:
         common.print_season_scores_with_header(
-            max_seasonal_points_for, "HIGHEST POINTS-FOR THIS SEASON",
+            results.max_season_scores, "HIGHEST POINTS-FOR THIS SEASON",
             seasonal_score_output_count)
-    if print_min_seasonal:
+    if results.min_season_scores:
         common.print_season_scores_with_header(
-            min_seasonal_points_for, "LOWEST POINTS-FOR THIS SEASON",
+            results.min_season_scores, "LOWEST POINTS-FOR THIS SEASON",
             seasonal_score_output_count)
 
 
