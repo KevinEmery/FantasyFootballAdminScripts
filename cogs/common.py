47e33dc796fb2c425d9469039c8cfb5d3bdcf7bd
Kevin Emery <360emery@gmail.com>
2024-07-07 20:51:53 -0700

    Port over the descriptive log helper
    Before we start bringing over actual functionality I want to make sure
    the foundational functions work. So this brings the pretty logging
    helper over to the cogs common directory for use throughout the cogs.

--- cogs/common.py
+++ cogs/common.py
@@ -1,1 +1,29 @@
+"""
+   Copyright 2024 Kevin Emery
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+from datetime import datetime
+
+
+# ID of my personal dev server
 DEV_SERVER_GUILD_ID = 1134308260812423168
+
+def print_descriptive_log(log_method: str, log_line: str = ""):
+    log_template = "{time:<20}{log_method:40.40}\t{log_line}"
+    formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(
+        log_template.format(time=formatted_time,
+                            log_method=log_method,
+                            log_line=log_line))
--- cogs/test.py
+++ cogs/test.py
@@ -1,17 +1,34 @@
+"""
+   Copyright 2024 Kevin Emery
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import discord
+
+import cogs.common as cogCommon
 
 from discord import app_commands
 from discord.ext import commands
-
-from .common import DEV_SERVER_GUILD_ID
 
 class TestCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name="helloworld", description="The only logical first command")
-    @app_commands.guilds(DEV_SERVER_GUILD_ID)
+    @app_commands.guilds(cogCommon.DEV_SERVER_GUILD_ID)
     async def testing(self, interaction: discord.Interaction):
+        cogCommon.print_descriptive_log("helloworld", "Log Testing!")
         await interaction.response.send_message("Hello, World!")
 
 async def setup(bot):
