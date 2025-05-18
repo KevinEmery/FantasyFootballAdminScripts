492c9f9a67dcb6c6b0835045e9e99054d7eac537
Kevin Emery <360emery@gmail.com>
2025-05-18 16:09:21 -0700

    Add logic to write logs out to a file
    Fixes #8

--- cogs/common.py
+++ cogs/common.py
@@ -1,5 +1,5 @@
 """
-   Copyright 2024 Kevin Emery
+   Copyright 2025 Kevin Emery
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,12 +15,27 @@
 """
 from datetime import datetime
 
+LOG_DIRECTORY_NAME = "./logs/"
+LOG_PREFIX = "log_"
 
-def print_descriptive_log(log_method: str, log_line: str = ""):
+def print_descriptive_log(log_method: str, log_line: str = "", write_to_file: bool = True):
     log_template = "{time:<20}{log_method:40.40}\t{log_line}"
     formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_date = datetime.now().strftime("%Y_%m_%d")
 
-    print(
-        log_template.format(time=formatted_time,
-                            log_method=log_method,
-                            log_line=log_line))
+    log_file_path = LOG_DIRECTORY_NAME + LOG_PREFIX + formatted_date
+    formatted_log = log_template.format(time=formatted_time,
+                                        log_method=log_method,
+                                        log_line=log_line)
+
+    print(formatted_log)
+
+    if write_to_file:
+        try:
+            with open(log_file_path, 'a+') as f:
+                f.write(formatted_log + "\n")
+        except Exception as e:
+            # This is non-critical infra, just log to console and move on.
+            print("Error writing log to file")
+            print(e)
+    
